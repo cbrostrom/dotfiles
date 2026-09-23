2115395463ca8bdbaa6ca2a7d9c5824143dba975
Christian Kjærulff Brostrøm <2435832+cbrostrom@users.noreply.github.com>
2026-09-23 11:56:51 +0200

    chore(paseo): drop duplicated plugin sources - canonical home is bybrostrom/paseo-plugins (git-installed)

--- sources/paseo/plugins/summary-fork/index.server.ts
+++
@@ -1,7 +0,0 @@
-import type { PluginServerContext } from "@getpaseo/plugin/server";
-import { forkSettings } from "./shared/summary-fork.js";
-
-export default function contribute(server: PluginServerContext) {
-  server.registerSettings(forkSettings);
-  return () => {};
-}
